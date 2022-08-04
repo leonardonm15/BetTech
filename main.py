56ac1153e4839bc3cd922baaaa5222056c4581ff
Enzo <enzo200325@gmail.com>
2022-08-04 01:06:07 -0300

    modified main (can now login automatically)

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from selenium import webdriver
-import chromedriver_autoinstaller
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import undetected_chromedriver.v2 as uc
+import time
 
-
-chromedriver_autoinstaller.install()
-
-driver = webdriver.Chrome()
-driver.get("https://livecasino.bet365.com/Play/LiveRoulette")
-assert "Python" in driver.title
+if __name__ == '__main__':
+    driver = uc.Chrome()
+    driver.get("https://livecasino.bet365.com/Play/LiveRoulette")
+    usernameInput = driver.find_element(By.ID, "txtUsername")
+    usernameInput.send_keys("ninjadocassino")
+    passwordInput = driver.find_element(By.ID, "txtPassword")
+    passwordInput.send_keys("cassino")
+    passwordInput.send_keys(Keys.RETURN)
+    time.sleep(60)
+    continueButton = driver.find_element(By.CLASS_NAME, "regulatory-last-login-modal__button")
+    continueButton.click()
+    time.sleep(60)
+    moreGamesButton = driver.find_element(By.CLASS_NAME, "header__more-games")
+    moreGamesButton.click()
 
