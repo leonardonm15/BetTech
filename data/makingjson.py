050275d7fd19cf8cc7de9eaef68390bf9a03bdb7
Enzo <enzo200325@gmail.com>
2022-08-14 23:03:31 -0300

    all patterns done, need testing

--- data/makingjson.py
+++ data/makingjson.py
@@ -9,8 +9,14 @@ data = {}
 for roulette in roulettes_needed:
     roulette_obj = {
         "patterns": {
+            "direta": [0 for _ in range(37)],
             "rua": [0 for _ in range(12)],
-            "canto": [[0 for _ in range(11)] for _ in range(2)]
+            "rua_dupla": [0 for _ in range(11)],
+            "canto": [[0 for _ in range(11)] for _ in range(2)],
+            "dupla": {
+                "rigth": [[0 for _ in range(11)] for _ in range(3)],
+                "down": [[0 for _ in range(12)] for _ in range(2)]
+            }
         },
         "numbers": []
     }
